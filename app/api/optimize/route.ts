eda59f378e84c287935b1158b05e81b5349f1f4f
kanokawashima <kawashima@c3brain.jp>
2026-06-24 11:23:38 +0900

    [ update ] pngのファイルサイズが削減できない件、調整

--- app/api/optimize/route.ts
+++ app/api/optimize/route.ts
@@ -67,6 +67,7 @@ export async function POST(request: NextRequest) {
         optimizedBuffer = await sharp(buffer)
           .png({
             compressionLevel: 9,
+            adaptiveFiltering: true,
           })
           .toBuffer();
 
